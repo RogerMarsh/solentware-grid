44610238432662fe3ebe350ae2679d4f7b9072fb
Roger Marsh <roger.marsh@solentware.co.uk>
2023-05-11 15:09:51 +0000

    Workaround problem inserting particular record types in Symas LMMD.
    Provide a 'put_no_refresh' method allowing 'insert record' and 'refresh
    record list' actions to be put in separate transactions if necessary.
    The equivalent split for 'edit' had already been done.
    
    New version of solentware-grid is 2.1.10.dev0.

--- solentware_grid/gui/dataedit.py
+++ solentware_grid/gui/dataedit.py
@@ -101,6 +101,17 @@ class RecordEdit(DataClient):
         edit proceeding if the record is changed elsewhere first.
 
         """
+        self.put_no_refresh(commit=commit)
+        self.datasource.refresh_widgets(self.newobject)
+
+    def put_no_refresh(self, commit=True):
+        """Insert the record without doing refresh widgets.
+
+        This is leading part of put split in an attempt to fix a problem
+        introduced as a consequence of explicit read-only transactions in
+        Symas LMMD.
+
+        """
         if commit:
             self.datasource.dbhome.start_transaction()
         self.newobject.put_record(
@@ -108,7 +119,6 @@ class RecordEdit(DataClient):
         )
         if commit:
             self.datasource.dbhome.commit()
-        self.datasource.refresh_widgets(self.newobject)
 
 
 class DataEdit(RecordEdit, Bindings):
